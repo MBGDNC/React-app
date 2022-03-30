7749ceb3dd76ec2f7988b6f7fa1154d18a7691cc
MBGDNC <m.bogdanic@icloud.com>
2022-03-30 20:43:00 +0200

    JSX

--- src/App.js
+++ src/App.js
@@ -12,7 +12,6 @@ function App() {
         <h1>Hi, I'm Mateo</h1>
         <p>Your unique ID is: <div className="random">{idHelpers.generateId()}</div></p>
         <RandomValue />
-        {/* <p>Your random array element is: {arrayHelpers.getRandomElement([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])}</p> */}
         <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
         <RepositoryLink />
         
