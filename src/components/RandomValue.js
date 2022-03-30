dc5c75395b56167915ddb283fcb3f0e2866c622c
MBGDNC <m.bogdanic@icloud.com>
2022-03-30 20:45:19 +0200

    JSX

--- src/components/RandomValue.js
+++ src/components/RandomValue.js
@@ -3,6 +3,6 @@ const randomNum = ["1", "2", "3"];
 export function RandomValue() {
   const i = Math.floor(Math.random() * randomNum.length);
   return (
-    <p>Your random number is: {randomNum[i]}</p>
+    <div>Your random number is: <div className="random">{randomNum[i]}</div></div>
   );
 }
